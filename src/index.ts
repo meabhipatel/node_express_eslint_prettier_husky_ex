e22ed6ade09cf65e8a344ab45f672f5df02d799e
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-06-27 11:26:31 +0530

    testing prettier formating with eslint

--- src/index.ts
+++ src/index.ts
@@ -3,37 +3,18 @@ const app = express();
 
 // const port = 3300;
 
-
-
-
-
-
-
-
 // const sometihng = "this is going to give error";
-
-
-
-
 
 // const some = "extap space"          +  " somthing"
 
 app.get("/", (req, res) => {
-
-
   res
     .status(200)
-    .json({ success: true, message: "Welcome to the home route."}   );
-
-
+    .json({ success: true, message: "Welcome to the home route." });
 });
 
 app.listen(3301, () => {
   console.log(`Server is running at http://localhost:3301`);
-
-  
 });
-
-
 
 //  some1 = "thingsa"
