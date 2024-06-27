004514a50446d091801e87d77e6899023ea02263
Abhishek Patel <abhishekp.wiz91@gmail.com>
2024-06-27 11:25:55 +0530

    added eslint-plugin-prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,18 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import eslintConfigPrettier from "eslint-config-prettier";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts}"] },
   { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  eslintConfigPrettier,
   {
     rules: {
-        "no-unused-vars": "error",
-        "no-undef": "error"
-    }
-}
+      "no-unused-vars": "error",
+      "no-undef": "error",
+    },
+  },
+  eslintPluginPrettierRecommended,
 ];
